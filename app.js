9907f940bc07c632ec30cb111a201b7a66ac92c7
JohnDorien1 <JohnDorien1@users.noreply.github.com>
2014-10-07 11:57:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Edit the lines in this block to match your local config. DO NOT EDIT BELOW THIS BLOCK!
+var syscoin = require('syscoin');
 
 var client = new syscoin.Client({
   host: 'localhost',
@@ -24,6 +25,7 @@ client.aliasList(function(err, result, resHeaders){
 
 for(var i = 0; i < aliases.length; i++){
   if(aliases[i].expiry <= 100){
+    // Do the wallet unlock clause here (one line, if locked --> unlock)
     client.aliasUpdate(aliases[i].name, aliases[i].value, function(err,result,resHeaders){
       if (err) return console.log(err);
       console.log(result);
