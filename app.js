b1cd0372c97725b9b35e35b61133551aaefbd04b
JohnDorien1 <JohnDorien1@users.noreply.github.com>
2014-10-10 19:36:28 +0200

    Implement breaking timestamp

--- app.js
+++ app.js
@@ -15,18 +15,19 @@ var islockedpw = 'mywalletpass';  // Change this to your wallet passphrase if en
 var updateall = 1; // change to 0 and update the array next line in case you want only certain aliases to be updated
 var specaliases = ["enter", "all", "aliases", "here"]; // case sensitive!!!
 
+var turnitofftimestamp = 12345; // Set the unix timestamp after which you do NOT want any more updates on your alias(es)
+
 /////////////////// END OF CONFIG ///////////////
 
 var aliases = '';
 var aliasinf = ''
 
+var actualtimestamp = (new Date).getTime();
+if (turnitofftimestamp >= actualtimestamp){console.log('Limit timestamp met. Aborting update'); break;};
 client.aliasList(function(err, result, resHeaders){
   if (err) return console.log(err);
   aliases = result;
   console.log('Result of aliases: ', aliases);  // Debug print for development
-  if(updateall == 0){
-    
-  }
   for(var i = 0; i < aliases.length; i++){
     if(aliases[i].expires_in <= 100){
       client.aliasInfo(aliases[i].name, function(err, result, resHeaders){
