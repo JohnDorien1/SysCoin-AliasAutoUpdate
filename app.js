f0cfc61cf366a58174aaa08891ab30fbad7f3531
JohnDorien1 <JohnDorien1@users.noreply.github.com>
2014-10-10 10:37:35 +0200

    implemented update of only given aliases

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var client = new syscoin.Client({
 var islocked = 0; // Change to 1 if your wallet is encrypted
 var islockedpw = 'mywalletpass';  // Change this to your wallet passphrase if encrypted
 
+var updateall = 1; // change to 0 and update the array next line in case you want only certain aliases to be updated
+var specaliases = ["enter", "all", "aliases", "here"]; // case sensitive!!!
 
 /////////////////// END OF CONFIG ///////////////
 
@@ -22,16 +24,22 @@ client.aliasList(function(err, result, resHeaders){
   if (err) return console.log(err);
   aliases = result;
   console.log('Result of aliases: ', aliases);  // Debug print for development
+  if(updateall == 0){
+    
+  }
   for(var i = 0; i < aliases.length; i++){
     if(aliases[i].expires_in <= 100){
       client.aliasInfo(aliases[i].name, function(err, result, resHeaders){
         aliasinf = result;
-        if(islocked == 1) client.walletpassphrase(islockedpw, 1000, function(err,result,resHeaders){});// Do the wallet unlock clause here (one line, if locked --> unlock)
-        client.aliasUpdate(aliasinf.name, aliasinf.value, function(err,result,resHeaders){
-          if (err) return console.log(err);
-          console.log(result);
-        });
-        if(islocked == 1) client.walletlock();
+        var check = specaliases.indexOf(aliasinf.name);
+        if(updateall == 0 && check != (-1)){
+          if(islocked == 1) client.walletpassphrase(islockedpw, 1000, function(err,result,resHeaders){});// Do the wallet unlock clause here (one line, if locked --> unlock)
+          client.aliasUpdate(aliasinf.name, aliasinf.value, function(err,result,resHeaders){
+            if (err) return console.log(err);
+            console.log(result);
+          });
+          if(islocked == 1) client.walletlock();
+        };
       });
     };
   };
